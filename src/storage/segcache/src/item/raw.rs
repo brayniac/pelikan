0782869beca8836e91a499647060a0cb5fa14b08
Brian Martin <bmartin@twitter.com>
2021-03-10 09:16:11 -0800

    add more docs

--- src/storage/segcache/src/item/raw.rs
+++ src/storage/segcache/src/item/raw.rs
@@ -1,6 +1,7 @@
 use crate::item::*;
 use crate::ITEM_HDR_SIZE;
 
+/// `RawItem` is the raw memory representation of an item.
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct RawItem {
@@ -8,23 +9,28 @@ pub struct RawItem {
 }
 
 impl RawItem {
+    /// Borrow the `ItemHeader`
     pub fn header(&self) -> &ItemHeader {
         unsafe { &*(self.data as *const ItemHeader) }
     }
 
+    /// Mutable borrow of the `ItemHeader`
     pub fn header_mut(&mut self) -> *mut ItemHeader {
         self.data as *mut ItemHeader
     }
 
+    /// Create a `RawItem` from a pointer
     pub fn from_ptr(ptr: *mut u8) -> RawItem {
         Self { data: ptr }
     }
 
+    /// Returns the key length
     #[inline]
     pub fn klen(&self) -> u8 {
         self.header().klen()
     }
 
+    /// Borrow the key
     pub fn key(&self) -> &[u8] {
         unsafe {
             let ptr = self.data.add(self.key_offset());
@@ -33,11 +39,13 @@ impl RawItem {
         }
     }
 
+    /// Returns the value length
     #[inline]
     pub fn vlen(&self) -> u32 {
         self.header().vlen()
     }
 
+    /// Borrow the value
     // TODO(bmartin): should probably change this to be Option<>
     pub fn value(&self) -> &[u8] {
         unsafe {
@@ -47,11 +55,13 @@ impl RawItem {
         }
     }
 
+    /// Returns the optional data length
     #[inline]
     pub fn olen(&self) -> u8 {
         self.header().olen()
     }
 
+    /// Borrow the optional data
     pub fn optional(&self) -> Option<&[u8]> {
         if self.olen() > 0 {
             unsafe {
@@ -64,11 +74,13 @@ impl RawItem {
         }
     }
 
+    /// Check the header magic bytes
     #[inline]
     pub fn check_magic(&self) {
         self.header().check_magic()
     }
 
+    /// Set the header magic bytes
     #[inline]
     pub fn set_magic(&mut self) {
         #[cfg(feature = "magic")]
@@ -77,6 +89,7 @@ impl RawItem {
         }
     }
 
+    /// Copy data into the item
     pub(crate) fn define(&mut self, key: &[u8], value: &[u8], optional: &[u8]) {
         unsafe {
             self.set_magic();
@@ -103,22 +116,25 @@ impl RawItem {
         }
     }
 
+    // Gets the offset to the optional data
     #[inline]
     fn optional_offset(&self) -> usize {
         ITEM_HDR_SIZE
     }
 
+    // Gets the offset to the key
     #[inline]
     fn key_offset(&self) -> usize {
         self.optional_offset() + self.olen() as usize
     }
 
+    // Gets the offset to the value
     #[inline]
     fn value_offset(&self) -> usize {
         self.key_offset() + self.klen() as usize
     }
 
-    // NOTE: size is rounded up for alignment
+    /// Returns item size, rounded up for alignment
     pub fn size(&self) -> usize {
         (((ITEM_HDR_SIZE + self.olen() as usize + self.klen() as usize + self.vlen() as usize)
             >> 3)
@@ -126,10 +142,12 @@ impl RawItem {
             << 3
     }
 
+    /// Sets the tombstone
     pub fn tombstone(&mut self) {
         unsafe { (*self.header_mut()).set_deleted(true) }
     }
 
+    /// Checks if the item is deleted
     pub fn deleted(&self) -> bool {
         self.header().is_deleted()
     }
