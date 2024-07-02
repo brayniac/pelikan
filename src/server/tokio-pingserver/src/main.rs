e9d3363571aee6125a663237c0e7f514b1195a94
Brian Martin <brayniac@gmail.com>
2024-07-02 13:59:15 -0700

    use same protocol impl for tokio pingserver as original

--- src/server/tokio-pingserver/src/main.rs
+++ src/server/tokio-pingserver/src/main.rs
@@ -1,8 +1,12 @@
+
 #[macro_use]
 extern crate logger;
 
-use protocol_ping::Response;
-use protocol_ping::Request;
+use std::io::ErrorKind;
+use ::config::BufConfig;
+use std::borrow::{Borrow, BorrowMut};
+use session::{Buf, BufMut, Buffer};
+use protocol_ping::{Compose, Parse, Request, Response};
 use server::ProcessBuilder;
 use entrystore::Noop;
 use protocol_ping::RequestParser;
@@ -167,37 +171,74 @@ fn tokio(config: Config, mut log: Box<dyn Drain>) {
                             continue;
                         }
 
+                        let buf_size = config.buf().size();
+
                         tokio::spawn(async move {
-                            let mut buf = vec![0; 1024];
+                            // initialize parser and the read and write bufs
+                            let parser = protocol_ping::RequestParser::new();
+                            let mut read_buffer = Buffer::new(buf_size);
+                            let mut write_buffer = Buffer::new(buf_size);
 
-                            // In a loop, read data from the socket and write the data back.
                             loop {
-                                buf.resize(1024, 0);
-
-                                match socket.read(&mut buf).await {
-                                    // socket closed
+                                // read from the socket
+                                match socket.read(read_buffer.borrow_mut()).await {
                                     Ok(0) => {
+                                        // socket was closed, return to close
                                         return;
                                     }
                                     Ok(n) => {
-                                        buf.truncate(n);
+                                        // bytes received, advance read buffer
+                                        // to make them available for parsing
+                                        unsafe {
+                                            read_buffer.advance_mut(n);
+                                        }
                                     }
                                     Err(_) => {
+                                        // some other error occurred, return to
+                                        // close
                                         return;
                                     }
                                 };
 
-                                match buf.as_slice() {
-                                    b"PING\r\n" | b"ping\r\n" => {
-                                        if socket.write_all(b"PONG\r\n").await.is_err() {
+                                // parse the read buffer
+                                let request = match parser.parse(read_buffer.borrow()) {
+                                    Ok(request) => {
+                                        // got a complete request, consume the
+                                        // bytes for the request by advancing
+                                        // the read buffer
+                                        let consumed = request.consumed();
+                                        read_buffer.advance(consumed);
+
+                                        request
+                                    }
+                                    Err(e) => match e.kind() {
+                                        ErrorKind::WouldBlock => {
+                                            // incomplete request, loop to read
+                                            // again
+                                            continue;
+                                        }
+                                        _ => {
+                                            // some parse error, return to close
                                             return;
                                         }
-                                    }
-                                    b => {
-                                        println!("buffer: {:?}", b);
-                                        return;
+                                    },
+                                };
+
+                                // compose a response into the write buffer
+                                match request.into_inner() {
+                                    Request::Ping => {
+                                        Response::Pong.compose(&mut write_buffer);
                                     }
                                 }
+
+                                // flush the write buffer, return to close on
+                                // error
+                                if socket.write_all(write_buffer.borrow()).await.is_err() {
+                                    return;
+                                }
+
+                                // clear the write buffer
+                                write_buffer.clear();
                             }
                         });
                     }
