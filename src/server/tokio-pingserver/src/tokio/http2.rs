940616469ed3453263583e0ac6e5bb462765aa80
Brian Martin <brayniac@gmail.com>
2024-08-02 17:06:51 -0700

    add debug statements

--- src/server/tokio-pingserver/src/tokio/http2.rs
+++ src/server/tokio-pingserver/src/tokio/http2.rs
@@ -12,72 +12,76 @@ pub async fn run(config: Arc<Config>) {
 
 	loop {
         if let Ok((stream, _)) = listener.accept().await {
-            if stream.set_nodelay(true).is_err() {
-                continue;
-            }
+            let _ = stream.set_nodelay(true).is_err();
 
             tokio::task::spawn(async move {
-                if let Ok(mut conn) = ::h2::server::handshake(stream).await {
-                    loop {
-                        match conn.accept().await {
-                            Some(Ok((request, mut sender))) => {
-                                tokio::spawn(async move {
-                                    let (_parts, mut body) = request.into_parts();
+                match ::h2::server::handshake(stream).await {
+                    Ok(mut conn) => {
+                        loop {
+                            match conn.accept().await {
+                                Some(Ok((request, mut sender))) => {
+                                    tokio::spawn(async move {
+                                        let (_parts, mut body) = request.into_parts();
 
-                                    let mut content = BytesMut::new();
-                                    while let Some(data) = body.data().await {
-                                        if data.is_err() {
-                                            eprintln!("couldn't read request body data");
-                                            return;
-                                        }
-                                        
-                                        let data = data.unwrap();
+                                        let mut content = BytesMut::new();
+                                        while let Some(data) = body.data().await {
+                                            if data.is_err() {
+                                                eprintln!("couldn't read request body data");
+                                                return;
+                                            }
+                                            
+                                            let data = data.unwrap();
 
-                                        content.extend_from_slice(&data);
-                                        let _ = body.flow_control().release_capacity(data.len());
+                                            content.extend_from_slice(&data);
+                                            let _ = body.flow_control().release_capacity(data.len());
 
-                                        if let Ok(_trailers) = body.trailers().await {
-                                            let now: DateTime<Utc> = Utc::now();
+                                            if let Ok(_trailers) = body.trailers().await {
+                                                let now: DateTime<Utc> = Utc::now();
 
-                                            let response = http::response::Builder::new()
-                                                .status(200)
-                                                .version(Version::HTTP_2)
-                                                .header("content-type", "application/grpc")
-                                                .header("date", now.to_rfc2822())
-                                                .body(())
-                                                .unwrap();
+                                                let response = http::response::Builder::new()
+                                                    .status(200)
+                                                    .version(Version::HTTP_2)
+                                                    .header("content-type", "application/grpc")
+                                                    .header("date", now.to_rfc2822())
+                                                    .body(())
+                                                    .unwrap();
 
-                                            let content = BytesMut::zeroed(5);
+                                                let content = BytesMut::zeroed(5);
 
-                                            let mut trailers = HeaderMap::new();
-                                            trailers.append("grpc-status", 0.into());
+                                                let mut trailers = HeaderMap::new();
+                                                trailers.append("grpc-status", 0.into());
 
-                                            if let Ok(mut stream) = sender.send_response(response, false) {
-                                                if stream.send_data(content.into(), false).is_ok() {
-                                                    if stream.send_trailers(trailers).is_err() {
-                                                        eprintln!("couldn't send response trailers");
+                                                if let Ok(mut stream) = sender.send_response(response, false) {
+                                                    if stream.send_data(content.into(), false).is_ok() {
+                                                        if stream.send_trailers(trailers).is_err() {
+                                                            eprintln!("couldn't send response trailers");
+                                                        }
+                                                    } else {
+                                                        eprintln!("couldn't send response content");
                                                     }
                                                 } else {
-                                                    eprintln!("couldn't send response content");
+                                                    eprintln!("couldn't send response headers");
                                                 }
                                             } else {
-                                                eprintln!("couldn't send response headers");
+                                                eprintln!("couldn't read trailers");
                                             }
-                                        } else {
-                                            eprintln!("couldn't read trailers");
                                         }
-                                    }
-                                });
-                            }
-                            Some(Err(e)) => {
-                                eprintln!("error: {e}");
-                                break;
-                            }
-                            None => {
-                                tokio::task::yield_now().await;
-                                continue;
+                                    });
+                                }
+                                Some(Err(e)) => {
+                                    eprintln!("error: {e}");
+                                    break;
+                                }
+                                None => {
+                                    eprintln!("yielding");
+                                    tokio::task::yield_now().await;
+                                    continue;
+                                }
                             }
                         }
+                    }
+                    Err(e) => {
+                        eprintln!("error during handshake: {e}");
                     }
                 }
             });
